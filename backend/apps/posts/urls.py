5a91ed61c622ed8d2d46685ff382c63553512515
D. Kalangiyam <d.kalangiyam@gmail.com>
2026-07-21 09:14:39 +0530

    feat(posts): implement post search API

--- backend/apps/posts/urls.py
+++ backend/apps/posts/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import include, path
 
-from .views import PostViewSet
+from .views import PostViewSet, PostSearchAPIView
 
 app_name = "posts"
 
@@ -11,4 +12,14 @@ router.register(
     basename="post",
 )
 
-urlpatterns = router.urls
+urlpatterns = [
+    path(
+        "search/",
+        PostSearchAPIView.as_view(),
+        name="post-search",
+    ),
+    path(
+        "",
+        include(router.urls),
+    ),
+]
